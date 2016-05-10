0c328275e279f6fc58cad3c2f4889236799c72da
lesage60400 <lesage.pierre@neuf.fr>
2016-05-10 09:55:40 +0200

    exercice 4 prairie PHP du 09/05/2016

---
+++ exercice4php.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>Fizz, Buzz et Fizzbuzz exercice 4 prairie du 09/05/2016</title>
+  </head>
+  <body>
+    <?php
+    $min = 1;
+    $max = 100;
+      for ($i = $min; $i <= $max; $i = $i + 1) {
+      if ($i%3 ==0){
+        echo "fizz";
+      }
+      elseif ($i%5 == 0){
+        echo "buzz";
+      }
+      elseif ($i%3 == 0and $i%5 == 0){
+        echo "fizzbuzz";
+      }
+      else{
+        echo $i;
+      }
+      }
+    ?>
+  </body>
+</html>
